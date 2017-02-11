adf59783b509cf0b985e0ab0135d66024f578620
Cory Gehr <corygehr@users.noreply.github.com>
2017-02-10 16:57:20 -0800

    Update CommandHandler.php

--- groupme-bot/models/BotResource/CommandHandler.php
+++ groupme-bot/models/BotResource/CommandHandler.php
@@ -319,6 +319,7 @@ class CommandHandler extends \Thinker\Framework\Model
 			    $context = stream_context_create($options);
 			    $result = file_get_contents("https://image.groupme.com/pictures", false, $context);
 			    $message->text = print_r($result, true);
+			    $message->send();
 			    return;
 			    // Get result URL
 			    $result_json = json_decode($result, true);
